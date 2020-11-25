4c519c552f99ea461f82c47604eca54c97223963
mesulimkhonza <mesuliskhanyisele@gmail.com>
2020-11-25 15:59:14 +0200

    quetions setups svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,15 @@
 <script>
-	import Heading from './Heading.svelte'
-	let days = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"];
-
+  import Heading from "./Heading.svelte";
+  import Questions from "./Questions.svelte";
+  
 </script>
-<Heading/>
-<main>
-{#each days as day}
-<button class="btn blue">{day}</button>
-<br>
-{/each}
-
-
-</main>
 
 <style>
 
 </style>
+
+<Heading />
+<main>
+  <Questions />
+
+</main>
---
+++ src/Questions.svelte
@@ -0,0 +1,46 @@
+<script>
+  let Q = [
+    {
+      question: "how do you stop the car",
+      options: [
+        "A. Press the hooter",
+        "B. Use the indicator",
+        "C. Use the brake"
+      ],
+      answer: "C"
+    },
+    {
+      question: "how do you start the car",
+      options: [
+        "A. Press the hooter",
+        "B. Use the indicator",
+        "C. Use the key"
+      ],
+      answer: "C"
+    }
+  ];
+
+  let QNum = 0;
+
+  function inc() {
+    QNum++;
+    if (QNum == 2) {
+      QNum = 0;
+    }
+    
+  }
+</script>
+
+<div>
+  <p>{Q[QNum].question}</p>
+  {#each Q[QNum].options as option}
+       <label>
+    <input type="radio" />
+    <span>{option}</span>
+  </label>
+  <br>
+  {/each}
+  
+
+  <button on:click={inc} class="btn">Next</button>
+</div>
